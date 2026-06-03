749290dcb5ff791a6343f91528edbd75f599dd8a
lmdoyle <leodoyle05@gmail.com>
2026-06-03 19:47:37 -0400

    Move inline scripts to js/twitch.js and js/youtube.js

---
+++ js/twitch.js
@@ -0,0 +1,8 @@
+new Twitch.Embed("twitch-embed", {
+  width: "100%",
+  height: 480,
+  channel: "gamer_guy_tv",
+  layout: "video",
+  autoplay: false,
+  parent: ["gamerguytv.com", "lmdoyle.github.io", "localhost"]
+});
